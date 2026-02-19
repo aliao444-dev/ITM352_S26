b95cc4fff1192c9e6d7a876c510b73ed77751557
aliao444-dev <aliao444@hawaii.edu>
2026-02-19 13:04:41 -1000

    s

--- Lab7/Ex2b.py
+++ Lab7/Ex2b.py
@@ -6,3 +6,9 @@ while (evens[-1] < 50):
     evens.append(num)
 
 print(evens)
+
+    # -1 tells it to go to the last element of the list
+    # doesn't work if the list is empty, so we start with 2 in the list and then add evens to it until we reach 50
+    # changing num to 4 allows us to start with the next even number after 2, which is 4, and then we keep adding 2 to get the next even numbers until we reach 50.
+# print(evens)
+
