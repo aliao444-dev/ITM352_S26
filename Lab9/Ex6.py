362beb230a9aab0a613d338283a74ccf028602b4
aliao444-dev <aliao444@hawaii.edu>
2026-03-13 19:49:32 -1000

    lab9 ex6

--- Lab9/Ex6.py
+++ Lab9/Ex6.py
@@ -8,14 +8,3 @@ file_path = os.path.join(os.path.dirname(__file__), "quiz_questions.json")
 with open(file_path, "r") as infile:
     quiz_questions = json.load(infile)
     print(json.dumps(quiz_questions, indent=4))
-
-# Advantages of JSON:
-# - Human-readable and easy to understand.
-# - Lightweight and widely used for data exchange.
-# - Supported by most programming languages.
-# - Can represent complex data structures (e.g., nested objects).
-
-# Disadvantages of JSON:
-# - No support for comments, which can make documentation harder.
-# - Limited data types (e.g., no support for dates or custom objects).
-# - Can be less efficient than binary formats for large datasets.
